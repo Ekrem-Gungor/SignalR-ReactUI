04fed6547f680ec1b0861316da29fe5d737c5d15
Ekrem Güngör <ekrmdsgnr@gmail.com>
2025-07-12 03:12:17 +0300

    deploy test 3

--- chat-app/vite.config.js
+++ chat-app/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/signalr-react-ui.vercel.app/",
   plugins: [react()],
   resolve: {
     alias: {
